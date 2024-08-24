ebf1affa29f1a3e6347ccde0d5f15de81803f2c1
Abdus Samad <imsamad00@gmail.com>
2024-08-24 15:34:02 +0530

    integrated cloudinary

--- server/src/index.js
+++ server/src/index.js
@@ -5,18 +5,35 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import fileUpload from 'express-fileupload';
+import path from 'path';
 
 import { notFound } from './middlewares/not-found.js';
 import { errorHandlerMiddleware } from './middlewares/error-handler-middleware.js';
 import { idCardRouters } from './router/idCardRouters.js';
 import { conenctDB } from './libs/connectDB.js';
+import { assetRouter } from './router/assetRouter.js';
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.disable('x-powered-by');
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(process.cwd(), '__tmp__'),
+    createParentPath: true,
+    safeFileNames: true,
+    uriDecodeFileNames: true,
+    uploadTimeout: 60_000,
+    preserveExtension: true,
+  })
+);
 
 app.use('/api/v1/idcards', idCardRouters);
+app.use('/api/v1/assets', assetRouter);
 app.get(['/health', '/'], (req, res) => {
   res.json({
     message: 'Server is running!',
